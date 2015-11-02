537d9a45d4734e7c307f7e1e926950ec41913598
Alexander Rajula <alexander@rajula.org>
2015-11-02 21:02:15 +0000

    Some sample memory regions.

--- data_distribution/distribution_areas.h
+++ data_distribution/distribution_areas.h
@@ -49,7 +49,8 @@ typedef enum{
   Katal,
   Fraction,
   Metres_Per_Second,
-  Newton_Metres
+  Newton_Metres,
+  Revolutions_Per_Second
 }data_unit;
 
 typedef struct{
@@ -57,6 +58,12 @@ typedef struct{
   data_encapsulation  encapsulation;
 }data_item_header;
 
-const char* ignition_angle_btdc = "/ignition_angle";
+const char* port_ignition_angle_btdc                = "/ignition_angle";
+const char* port_engine_torque                      = "/engine_torque";
+const char* port_engine_speed                       = "/engine_speed";
+const char* port_engine_power                       = "/engine_power";
+const char* port_cylinder_head_coolant_temperature  = "/cylinder_head_coolant_temperature";
+const char* port_intake_air_flow_rate               = "/intake_mass_air_flow_rate";
+const char* port_intake_air_temperature             = "/intake_air_temperature";
 
 #endif
