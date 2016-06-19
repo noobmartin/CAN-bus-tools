4067d8ae87153ed6fadf7bc4157c016dcb46e3d1
Alexander Rajula <alexander@rajula.org>
2016-06-19 12:28:32 +0200

    Added another abstraction to the library by introducing a class representing a node connected to the CAN bus.
    This node is identified by a textual string name, and may be configured
    to be a provider of a set of CAN frame identifiers.
    
    The node abstraction may be useful if you want to maintain a tree of CAN
    nodes, and filter CAN bus traffic based on who transmitted the data.

---
+++ libraries/can/node.cpp
@@ -0,0 +1,58 @@
+/*
+ * Author: Alexander Rajula
+ * E-mail: superrajula@gmail.com
+ * Descrpition:
+ *   Implementation file for the node class.
+ */
+
+#include "node.hpp"
+
+namespace can{
+
+node::node(){
+  memset(name, 0x0, MAX_NODENAME_SIZE);
+  memset(provided_frame_identifiers, 0x0, MAX_PROVIDED_FRAME_IDENTIFIERS*sizeof(unsigned int));
+  number_of_provided_frame_identifiers = 0x0;
+}/*node::node*/
+
+node::~node(){
+
+}/*node::~node*/
+
+int node::set_name(const unsigned size, const char* given_name){
+  if((given_name == 0) || size < 1){
+    perror("Cannot set name");
+    return -1;
+  }/*if*/
+
+  memcpy(name, given_name, size);
+}/*node::set_name*/
+
+int node::add_provided_frame(const unsigned int identifier){
+  if(number_of_provided_frame_identifiers >= MAX_PROVIDED_FRAME_IDENTIFIERS){
+    return -1;
+  }/*if*/
+
+  provided_frame_identifiers[number_of_provided_frame_identifiers] = identifier;
+
+  number_of_provided_frame_identifiers += 1;
+
+  return 0;
+
+}/*node::add_provided_frame*/
+
+int node::initialize(const char* canbus_name){
+  canbus::set_name(canbus_name);
+  canbus::open();
+
+  for(int i = 0; i < number_of_provided_frame_identifiers; i++){
+    canbus::add_receive_frame_filter(provided_frame_identifiers[i], CAN_SFF_MASK);
+  }/*for*/
+
+}/*node::initialize*/
+
+int get_provided_data(const unsigned size, char* data_buffer, unsigned int* identifier){
+  return canbus::receive(size, data_buffer, identifier);
+}/*node::get_provided_data*/
+
+}
---
+++ libraries/can/node.hpp
@@ -0,0 +1,80 @@
+/*
+ * Author: Alexander Rajula
+ * E-mail: superrajula@gmail.com
+ * Description:
+ *  This class represents a node connected to a CAN bus.
+ *  Each node may be the originator of a number of CAN frame identifiers,
+ *  also called arbitration IDs.
+ *
+ *  The list of frame identifiers is used to filter incoming traffic to only match
+ *  the set of identifiers associated with this node.
+ */
+
+#ifndef _node_hpp_
+#define _node_hpp_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <linux/can/bcm.h>
+
+#include "can/bus.hpp"
+
+namespace can{
+
+#define MAX_NODENAME_SIZE               256
+#define MAX_PROVIDED_FRAME_IDENTIFIERS  256
+
+class node{
+  private:
+    char          name[MAX_NODENAME_SIZE];
+    unsigned int  provided_frame_identifiers[MAX_PROVIDED_FRAME_IDENTIFIERS];
+    unsigned int  number_of_provided_frame_identifiers;
+
+    can::bus      canbus;
+
+  public:
+    node();
+    ~node();
+
+    /*
+     * Use this method if you want to associate this node with a
+     * textual string name.
+     */
+    int set_name(const unsigned size, const char* name);
+
+    /*
+     * Use this method during initialization of the node.
+     * If the node provides several frames with different identifiers,
+     * call this method several times, in sequence, with the set of 
+     * provided identifiers associated with this node.
+     */
+    int add_provided_frame(const unsigned int identifier);
+
+    /*
+     * Once the name, applicable filters etc. have been set up,
+     * call this method to connect this node to the CAN bus.
+     * This method has to be called before any data from the 
+     * node may be retrieved.
+     */
+    int initialize(const char* canbus_name);
+
+    /*
+     * Use this method to fetch data from this node.
+     * The number of bytes fetched is returned.
+     */
+    int get_provided_data(const unsigned size, char* data_buffer, unsigned int* identifier);
+};
+
+}
+
+#endif
